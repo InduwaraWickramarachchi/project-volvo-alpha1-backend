cfc841b3057f295ab2cf7b0552ee4c5e0568009f
Ajax <kasun@leoera.com>
2019-02-07 16:27:15 +0530

    Initial Commit

---
+++ src/main/java/com/leoera/projectvolvoalpha1backend/ProjectVolvoAlpha1BackendApplication.java
@@ -0,0 +1,14 @@
+package com.leoera.projectvolvoalpha1backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectVolvoAlpha1BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectVolvoAlpha1BackendApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/leoera/projectvolvoalpha1backend/ProjectVolvoAlpha1BackendApplicationTests.java
@@ -0,0 +1,17 @@
+package com.leoera.projectvolvoalpha1backend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectVolvoAlpha1BackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
